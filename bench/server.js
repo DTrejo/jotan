0e935f9255543daff7d1ab1578bfddefa02bd16c
David Trejo <david.daniel.trejo@gmail.com>
2012-06-18 16:02:59 -0700

    deploy to jitsu for benching

--- bench/server.js
+++ bench/server.js
@@ -1,14 +1,19 @@
+//
+// http
+//
+require('http').createServer(function(req, res) {
+  res.end('numMessages: ' + numMessages)
+}).listen(8000)
+console.log('http server> listening on http://localhost:8000')
+
+//
+// tcp
+//
 require('../').createServer(onConnection, { timeout: 5000 })
 
 var numMessages = 0;
-
 function onConnection(_, messageStream) {
   messageStream.on('data', function(buf) {
     numMessages++
   })
 }
-
-require('http').createServer(function(req, res) {
-  res.end('numMessages: ' + numMessages)
-}).listen(8000)
-console.log('http server> listening on http://localhost:8000')
